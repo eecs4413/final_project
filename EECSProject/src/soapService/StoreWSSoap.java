51eb2f2114d571a6ca923f844e5d4c361943a765
GlsgwAkeem <dwell.akeem@gmail.com>
2018-03-19 13:01:49 -0400

    - completed soap class

--- EECSProject/src/soapService/StoreWSSoap.java
+++ EECSProject/src/soapService/StoreWSSoap.java
@@ -1,10 +1,16 @@
 package soapService;
 
+import java.io.StringWriter;
 import java.util.Map;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.transform.stream.StreamResult;
 
 import bean.BookBean;
 import dao.BookDAO;
-
+// This is complete
 public class StoreWSSoap {
 
 	Map<String, BookBean> catalog;
@@ -15,7 +21,25 @@ public class StoreWSSoap {
 	}
 
 	public String getProductInfo(String productId) {
-		return catalog.get(productId).toString();
+
+		try {
+			JAXBContext jc = JAXBContext.newInstance(catalog.get(productId).getClass());
+			Marshaller marshaller = jc.createMarshaller();
+
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+
+			StringWriter sw = new StringWriter();
+			sw.write("\n");
+
+			marshaller.marshal(catalog.get(productId), new StreamResult(sw));
+			return sw.toString();
+
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
